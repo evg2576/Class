a913b233df51fe06f134e45b120ee8aaf28e381c
sanynikonov <nikonov.sanynikonov@gmail.com>
2020-04-17 14:30:24 +0000

    Update ClassTask.cs

--- Class/ClassTask.cs
+++ Class/ClassTask.cs
@@ -1,30 +1,32 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Class
 {
-    //TODO: Create class Rectangle here
+    //TODO: Create public class Rectangle here
 
-    //TODO: Define two private double fields: sideA, sideB
-    //TODO: Define constructor with two double parameters: a, b. Assign parameters to sides
-    //TODO: Define constructor with double parameter: a. Parameter sets side A of rectangle, and B-side always equals 5
+    //TODO: Define two private double fields: 'sideA', 'sideB'
+    //TODO: Define constructor with two double parameters: 'a', 'b'. Assign parameters to sides
+    //TODO: Define constructor with double parameter: 'a'. Parameter sets side A of rectangle, and B-side always equals 5
     //TODO: Define constructor without parameters. A-side of rectangle equals 4, B-side - 3
-    //TODO: Define method that returns value of A-side: GetSideA
-    //TODO: Define method that returns value of B-side: GetSideB
-    //TODO: Define method that calculates and returns value of area: Area
-    //TODO: Define method that calculates and returns value of perimeter: Perimeter
-    //TODO: Define method that checks if rectangle is a square: IsSquare. Returns true if it is a square, false if it's not.
-    //TODO: Define method that swaps values of rectangle sides: ReplaceSides
+    //TODO: Define public method 'GetSideA' that returns value of A-side
+    //TODO: Define public method 'GetSideB' that returns value of B-side
+    //TODO: Define public method 'Area' that calculates and returns value of area
+    //TODO: Define public method 'Perimeter' that calculates and returns value of perimeter
+    //TODO: Define public method 'IsSquare' that checks if rectangle is a square. Returns true if it is a square, false if it's not.
+    //TODO: Define public method 'ReplaceSides' that swaps values of rectangle sides
 
 
 
-    //TODO: Create class ArrayRectangles here
+    //TODO: Create public class ArrayRectangles here
 
     //TODO: Define private field that is array of rectangles: rectangle_array
-    //TODO: Define constructor with gets parameter: n. Constructor should create an empty array of rectangles with length of n
+    //TODO: Define constructor with int parameter: 'n'. Constructor should create an empty array of rectangles with length of 'n'
     //TODO: Define constructor that gets enumerable or array of rectangles. Constructor should assign them to its field
-    //TODO: Define method that adds rectangle on the first empty place of array field: AddRectangle. Returns true if array has empty space, if not
-    //TODO: Define method that returns number of rectangle with max value of area: NumberMaxArea. Numbering starts from 0
-    //TODO: Define method that returns number of rectangle with min value of perimeter: NumberMinPerimeter. Numbering starts from 0
-    //TODO: Define method that returns amount of squares in array of rectangles: NumberSquare
+    //TODO: Define public method 'AddRectangle' that adds rectangle on the first empty place of array field. Returns true if array has empty space, if not
+    //TODO: Define public method 'NumberMaxArea' that returns number of rectangle with max value of area. Numbering starts from 0
+    //TODO: Define public method 'NumberMinPerimeter' that returns number of rectangle with min value of perimeter. Numbering starts from 0
+    //TODO: Define public method 'NumberSquare' that returns amount of squares in array of rectangles
 
 }
